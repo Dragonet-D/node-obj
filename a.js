7c673e56cefd502fcf4cd2b0b9f8ea00b36e4d54
Deng XiaoLong <Dragonet_d@163.com>
2018-11-24 22:57:17 +0800

    init express object

---
+++ a.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = new express();
+app.set("view engine", "ejs");
+
+// 配置public目录为静态资源目录
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/product", (req, res) => {
+  res.render("product");
+});
+
+app.listen(3000, "localhost");
