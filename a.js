b274f31ac095bc67392bab255e09ecad24dea6e9
Deng XiaoLong <Dragonet_d@163.com>
2018-11-24 23:56:26 +0800

    login function came true

--- a.js
+++ a.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
+const DBUrl = "mongodb://localhost:27018";
 const app = new express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 // 配置public目录为静态资源目录
@@ -12,6 +17,25 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.post("/doLogin", (req, res) => {
+  const { username, password } = req.body;
+  // 获取数据 连接数据库查询数据
+  MongoClient.connect(DBUrl, {useNewUrlParser: true}, (err, db) => {
+    if (err) throw err;
+    // 查询数据
+    const dbo = db.db("productmanage");
+    dbo.collection("user").find({username, password}).toArray((error, result) => {
+      console.log(result);
+      if (result.length) {
+        res.redirect('/product');
+      } else {
+        res.send('<script>location.href="/login"</script>')
+      }
+      db.close();
+    })
+  });
 });
 
 app.get("/product", (req, res) => {
