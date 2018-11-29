4683cf845acd93e72e619c885eae7deeed18de43
Deng XiaoLong <Dragonet_d@163.com>
2018-11-29 22:03:36 +0800

    use await next for middleware

--- koa-obj/koa-test/server/index.js
+++ koa-obj/koa-test/server/index.js
@@ -1,9 +1,23 @@
 const Koa = require("koa");
-
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  ctx.body = "Hi Luke"
-});
+const mid1 = async (ctx, next) => {
+  ctx.type = "text/html; charset=utf-8"
+  await next();
+};
+
+const mid2 = async (ctx, next) => {
+  ctx.body = "Hi";
+  await next();
+};
+
+const mid3 = async (ctx, next) => {
+  ctx.body = ctx.body + "Luke";
+  // await next();
+};
+
+app.use(mid1);
+app.use(mid2);
+app.use(mid3);
 
 app.listen(3000);
