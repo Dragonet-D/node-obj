06ddacea79daf1f269bb033819c6909453eeec69
Deng XiaoLong <Dragonet_d@163.com>
2019-10-07 23:02:53 +0800

    :alien: koa source code study

--- koa-obj/kob/kob.js
+++ koa-obj/kob/kob.js
@@ -3,4 +3,25 @@ const context = require('./context');
 const request = require('./request');
 const response = require('./response');
 
+class Kob {
+  constructor() {
+    this.middlewares = [];
+  }
 
+  listen(..args) {
+    http
+    .createServer(async (req, res) => {
+      // 创建上下文对象
+      const ctx = this.createContext(req, res);
+      // 将中间件变成一个
+      const fn = this.compose(this.middlewares)
+      await fn(ctx)
+      res.end(ctx.body)
+    })
+    .listen(...args);
+  }
+  use(mid) {
+    this.middlewares.push(mid);
+  }
+  
+}
